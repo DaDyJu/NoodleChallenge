35ed8b7689f3fac0920d69227ec1142d79a35006
Junyu Zhou <jzhou@zynga.com>
2017-04-20 15:10:55 -0400

    hi Daniel

--- app/src/main/java/com/kkadadeepju/snwf/sendnoodswithfriends/GameActivity.java
+++ app/src/main/java/com/kkadadeepju/snwf/sendnoodswithfriends/GameActivity.java
@@ -5,6 +5,9 @@ import android.os.CountDownTimer;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,17 +20,44 @@ public class GameActivity extends AppCompatActivity {
     private TextView playerTwoScore;
     private TextView playerThreeScore;
     private TextView playerFourScore;
+    private ImageView noodleBowl;
+    private ImageView chopStickUp;
+    private ImageView chopStickDown;
+
+    private boolean isChopstickUp = false;
 
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
         timer = (TextView) findViewById(R.id.timer);
         playerTwoScore = (TextView) findViewById(R.id.player_two_score);
         playerThreeScore = (TextView) findViewById(R.id.player_three_score);
         playerFourScore = (TextView) findViewById(R.id.player_four_score);
 
-        new CountDownTimer(60000, 1000) {
+        chopStickUp = (ImageView) findViewById(R.id.chopstick_up);
+        chopStickDown = (ImageView) findViewById(R.id.chopstick_down);
+        noodleBowl = (ImageView) findViewById(R.id.noodle_bowl);
+
+        noodleBowl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("Junyu", "1");
+                if (isChopstickUp) {
+                    chopStickUp.setVisibility(View.GONE);
+                    chopStickDown.setVisibility(View.VISIBLE);
+                    isChopstickUp = false;
+                } else {
+                    chopStickUp.setVisibility(View.VISIBLE);
+                    chopStickDown.setVisibility(View.GONE);
+                    isChopstickUp = true;
+                }
+            }
+        });
+
+
+        new CountDownTimer(30000, 1000) {
 
             public void onTick(long millisUntilFinished) {
                 timer.setText("seconds remaining: " + millisUntilFinished / 1000);
