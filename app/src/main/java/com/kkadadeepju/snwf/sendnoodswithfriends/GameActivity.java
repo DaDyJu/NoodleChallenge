8796763834739d9046d39141fa930bd05a167cb7
Dylan Porte-Trachsel <dportetrachsel@zynga.com>
2017-04-21 13:05:25 -0400

    Added audio to app

--- app/src/main/java/com/kkadadeepju/snwf/sendnoodswithfriends/GameActivity.java
+++ app/src/main/java/com/kkadadeepju/snwf/sendnoodswithfriends/GameActivity.java
@@ -3,6 +3,7 @@ package com.kkadadeepju.snwf.sendnoodswithfriends;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.PersistableBundle;
@@ -20,6 +21,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by jzhou on 2017-04-20.
@@ -34,6 +36,7 @@ public class GameActivity extends AppCompatActivity {
     private ImageView noodleBowl;
     private ImageView chopStickUp;
     private ImageView chopStickDown;
+    private MediaPlayer mediaPlayer;
 
     private int score = 0;
 
@@ -69,6 +72,8 @@ public class GameActivity extends AppCompatActivity {
         chopStickDown = (ImageView) findViewById(R.id.chopstick_down);
         noodleBowl = (ImageView) findViewById(R.id.noodle_bowl);
 
+        mediaPlayer = new MediaPlayer();
+
         for (int i = 0; i < bowls.length; i++) {
             images.add(ContextCompat.getDrawable(getApplicationContext(), bowls[i]));
         }
@@ -78,6 +83,8 @@ public class GameActivity extends AppCompatActivity {
         noodleBowl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                playOiSound();
+
                 playerScore.setText(Integer.toString(++score));
                 playerScore.startAnimation(pulse);
 
@@ -119,4 +126,22 @@ public class GameActivity extends AppCompatActivity {
                 }.start();
     }
 
+    public void playOiSound() {
+        mediaPlayer.stop();
+
+        Random rand = new Random();
+        int num = rand.nextInt(3) +1;
+
+        if (num == 1) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.oi_1);
+        }
+        else if (num == 2) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.oi_2);
+        }
+        else if (num == 3) {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.oi_3);
+        }
+
+        mediaPlayer.start();
+    }
 }
